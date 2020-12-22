2c593f0d7a3978aa628c388ae82cade6d427885c
FAL <falworks.contact@gmail.com>
2020-12-23 05:38:25 +0900

    remove old code

--- src/build/index.ts
+++
@@ -1,8 +0,0 @@
-if (
-  require.resolve("./format") &&
-  require.resolve("./rollup") &&
-  require.resolve("./terser") &&
-  require.resolve("./typescript")
-) {
-  exports.browserModule = require("./browser-module");
-}
