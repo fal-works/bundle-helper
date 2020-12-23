6422bd05d8265864a187970ad6e50157cc4d29b4
FAL <falworks.contact@gmail.com>
2020-12-23 18:50:29 +0900

    add dogfooding.js

---
+++ test/dogfooding.js
@@ -0,0 +1,11 @@
+if (!require("fs").existsSync("lib/index.js")) {
+  console.error("[!] Library files not found. First build with npm scripts.");
+  process.exit(1);
+}
+
+const builder = require("../lib/build/node-module");
+
+builder.run({
+  distDir: "lib",
+  typesDir: "types",
+});
