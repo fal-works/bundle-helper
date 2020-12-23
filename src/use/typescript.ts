3c486ea71e3742437c3a49fdc1b2fd92b2674e23
FAL <falworks.contact@gmail.com>
2020-12-23 18:14:16 +0900

    ts: add recommended options at default

--- src/use/typescript.ts
+++ src/use/typescript.ts
@@ -2,12 +2,32 @@ import { cmd, exec, types } from "@fal-works/s-l-t-r";
 
 export interface TscConfig {
   tscArgs?: string[];
+  tscAddRecommendedOptions?: boolean;
 }
 
+const recommendedOptions: readonly string[] = [
+  "--alwaysStrict",
+  "--skipLibCheck",
+  "--noStrictGenericChecks",
+  "--newLine lf",
+];
+
 /** Immediately runs `tsc`. */
-export const execute = (tscArgs?: string[]): Promise<void> =>
-  tscArgs ? exec("tsc", ...tscArgs) : exec("tsc");
+export const execute = (
+  tscArgs: string[] = [],
+  addRecommendedOptions = true
+): Promise<void> => {
+  const options = tscArgs.concat(
+    addRecommendedOptions ? recommendedOptions : []
+  );
+  return exec("tsc", ...options);
+};
 
 /** @returns `Command` object that runs `tsc`. */
-export const command = (config: TscConfig): types.Command =>
-  config.tscArgs ? cmd("tsc", ...config.tscArgs) : cmd("tsc");
+export const command = (config: TscConfig): types.Command => {
+  const userOptions = config.tscArgs || [];
+  const recommended =
+    config.tscAddRecommendedOptions !== false ? recommendedOptions : [];
+  const name = ["tsc"].concat(userOptions).join(" ");
+  return cmd("tsc", ...userOptions.concat(recommended)).rename(name);
+};
